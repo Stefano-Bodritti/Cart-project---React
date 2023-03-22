22c713e39693840e62aaa6723247564e94ea90b9
Stefano <tepo@hotmail.it>
2023-03-22 10:22:13 +0100

    add empy cart functionality

--- src/reducer.js
+++ src/reducer.js
@@ -1,5 +1,10 @@
 const reducer = (state, action) => {
-  return state;
+
+  if (action.type === "CLEAR_CART") {
+    return {...state, cart: []}
+  }
+  
+  return state
 }
 
 export default reducer;
